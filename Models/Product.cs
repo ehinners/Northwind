9a24509cc145a0f5fa6791a351d647f377e6d0ec
ehinners <ehinners@my.wctc.edu>
2022-02-21 22:13:03 -0600

    Database Populated

--- Models/Product.cs
+++ Models/Product.cs
@@ -10,6 +10,8 @@ namespace Northwind.Models
         public  string QuantityPerUnit {get; set;}
         public  decimal UnitPrice {get; set;}
         public  short UnitsInStock {get; set;}
+
+        public short UnitsOnOrder{get; set;}
         public  short ReorderLevel {get; set;}
         public  bool Discontinued {get; set;}
 
