23082a945e3583a850f40d16b5b1483216b72901
ehinners <ehinners@my.wctc.edu>
2022-02-28 12:19:21 -0600

    Proper Dropdown link and product detail pages

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Models;
+using System.Linq;
 
 namespace Northwind.Controllers
 {
@@ -9,5 +10,11 @@ namespace Northwind.Controllers
         private NorthwindContext _northwindContext;
         public ProductController(NorthwindContext db) => _northwindContext = db;
         public ActionResult Category() => View(_northwindContext.Categories);
+
+        public IActionResult ProductDetail(int id) => View(new ProductViewModel
+        {
+            category = _northwindContext.Categories.FirstOrDefault(b => b.CategoryId == id),
+            Products = _northwindContext.Products.Where(p => p.CategoryId == id)
+        });
     }
 }
