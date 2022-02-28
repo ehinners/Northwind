0b87033adacb39f5e39b2ec9b0c2b31f254f6c24
ehinners <ehinners@my.wctc.edu>
2022-02-28 11:21:37 -0600

    Sort Categories By Name

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 using System;
+using Northwind.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Northwind.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index() => View();
+        private NorthwindContext _northwindContext;
+        public HomeController(NorthwindContext db) => _northwindContext = db;
+        public IActionResult Index() => View(_northwindContext.Categories.OrderBy(b => b.CategoryName));
     }
 }
