24c229b4067f6b4b36436168511d905e4ca71886
ehinners <ehinners@my.wctc.edu>
2022-03-07 20:49:05 -0600

    Product Name In Toast

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Models;
 using System.Linq;
+using System.Collections.Generic;
 namespace Northwind.Controllers
 {
     public class HomeController : Controller
@@ -11,8 +12,24 @@ namespace Northwind.Controllers
         public HomeController(NorthwindContext db) => _northwindContext = db;
 
         
-        public ActionResult Index() => View(
-            _northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3)
-            );
+        public ActionResult Index()
+{
+    IEnumerable<Discount> discounts = (from discount in _northwindContext.Discounts
+    join product in _northwindContext.Products on discount.ProductID equals product.ProductId
+    where discount.StartTime <= DateTime.Now && discount.EndTime > DateTime.Now
+    select new Discount { 
+        DiscountID = discount.DiscountID,
+        Code = discount.Code,
+        StartTime = discount.StartTime,
+        EndTime = discount.EndTime,
+        ProductID = discount.ProductID,
+        DiscountPercent = discount.DiscountPercent,
+        Title = discount.Title,
+        Description = discount.Description,
+        Product = product
+    }).Take(3).ToList();
+
+    return View(discounts);
+}
     }
 }
