a883d0e3e345412c864c313548d2484369860ad1
ehinners <ehinners@my.wctc.edu>
2022-03-08 00:04:42 -0600

    Discount Page

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Models;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Northwind.Controllers
 {
@@ -16,5 +17,25 @@ namespace Northwind.Controllers
             category = _northwindContext.Categories.FirstOrDefault(b => b.CategoryId == id),
             Products = _northwindContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false)
         });
+
+        public ActionResult DiscountDetail()
+        {
+            IEnumerable<Discount> discounts = (from discount in _northwindContext.Discounts
+            join product in _northwindContext.Products on discount.ProductID equals product.ProductId
+            where discount.StartTime <= DateTime.Now && discount.EndTime > DateTime.Now
+            select new Discount { 
+                DiscountID = discount.DiscountID,
+                Code = discount.Code,
+                StartTime = discount.StartTime,
+                EndTime = discount.EndTime,
+                ProductID = discount.ProductID,
+                DiscountPercent = discount.DiscountPercent,
+                Title = discount.Title,
+                Description = discount.Description,
+                Product = product
+            }).ToList();
+
+            return View(discounts);
+        }
     }
 }
---
+++ Models/DiscountViewModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace Northwind.Models
+{
+    public class DiscountViewModel
+    {
+        public IEnumerable<Discount> Discounts { get; set; }
+    }
+}
