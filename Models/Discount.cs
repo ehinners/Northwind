18794ecd98b504a6338b614e5b423f850cfc2203
Alijah <aperez27@my.wctc.edu>
2022-05-07 13:49:19 -0600

    Changed editDiscount View

--- Models/Discount.cs
+++ Models/Discount.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace Northwind.Models
 {
@@ -15,11 +16,7 @@ namespace Northwind.Models
         public DateTime EndTime { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
         public int ProductID { get; set; }
-<<<<<<< Updated upstream
         [Range(0, 1.00, ErrorMessage = "Please enter a value between {1} and {2}")]
-=======
-
->>>>>>> Stashed changes
         public decimal DiscountPercent { get; set; }
         [Required]
         public string Title { get; set; }
@@ -27,5 +24,8 @@ namespace Northwind.Models
         public string Description { get; set; }
 
         public Product Product { get; set; }
+
+        ICollection<Product> Products {get; set;}
+
     }
 }
