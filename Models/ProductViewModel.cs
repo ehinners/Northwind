f7ba0009f95a6f0fb7f38c96138e1f7856ed95d9
ehinners <ehinners@my.wctc.edu>
2022-05-07 16:30:18 -0500

    Small Reverts

--- Models/ProductViewModel.cs
+++ Models/ProductViewModel.cs
@@ -5,8 +5,6 @@ namespace Northwind.Models
     public class ProductViewModel
     {
         public Category category { get; set; }
-
-        public Discount discount { get; set; }
         public IEnumerable<Product> Products { get; set; }
     }
 }
